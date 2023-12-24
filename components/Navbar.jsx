520eb673a5a3269b2d1139a658ba069962e30231
MERAJ HASSAN <101815583+merajafnan@users.noreply.github.com>
2023-12-24 17:46:33 +0530

    sign_in_sign_out_created

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,29 @@
+ 'use client';
  import Link from "next/link";
+ import { signIn, signOut, useSession } from "next-auth/react"; // To check uer is logged in 
 
  function Navbar () {
+    const {status} = useSession();
+    console.log(status);
     return (
         <div className="p-4 flex justify-between items-center shadow-lg">
             <Link className="font-bold text-lg text-blue-700" href={"/"}>GoogleAuthTest</Link>
-            <button className="bg-slate-900 text-white px-6 py-2 rounded-md">Sign In</button>
+            {status === 'authenticated' ? 
+            (
+                <button 
+                    className="bg-slate-900 text-white px-6 py-2 rounded-md"
+                    onClick={() => signOut()}
+                    >
+                    Sign Out
+                    </button>
+            ) : 
+                <button 
+                    className="bg-slate-900 text-white px-6 py-2 rounded-md"
+                    onClick={() => signIn('google')}
+                    >
+                    Sign In
+                    </button>
+            }
         </div>
     ) 
  }
--- components/SignInBtn.jsx
+++ components/SignInBtn.jsx
@@ -1,8 +1,13 @@
+'use client';
 import Image from "next/image";
+import { signIn } from "next-auth/react";
 
 function SignInBtn() {
     return(
-        <button className="flex items-center gap-4 shadow-xl rounded-lg pl-3" >
+        <button 
+        className="flex items-center gap-4 shadow-xl rounded-lg pl-3" 
+        onClick={() => signIn("google")}
+        >
         <Image src='/google-sign-in.png' height={50} width={50}/>
         <span className="bg-blue-500 text-white px-4 py-3" > Sign in with Google </span>
        

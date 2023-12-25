15ddc719d697d56f6ed48fec3eed0f9be869182b
MERAJ HASSAN <101815583+merajafnan@users.noreply.github.com>
2023-12-25 14:15:53 +0530

    Google auth app completed

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
+import { connectMongoDB } from "@/lib/mongodb";
 import nextAuth from "next-auth";
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google';
+import User from "@/models/user";
 
 const authOptions = {
     providers: [
@@ -8,10 +10,43 @@ const authOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
         })
-    ]
+    ],
+    callbacks: {
+        async signIn({ user, account }) {
+            console.log("User: ", user);
+            console.log("Account: ", account);
+
+            if (account.provider === "google") {
+                const { name, email } = user;
+                try {
+                    await connectMongoDB();
+                    const userExists = await User.findOne({ email });
+
+                    if (!userExists) {
+                        const res = await fetch('http://localhost:3000/api/user', {
+                            method: 'POST',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            },
+                            body: JSON.stringify({ name, email })
+                        });
+    
+                        if (res.ok) {
+                            return user;
+                        }
+                    }
+
+                } catch (error) {
+                    console.log(error);
+
+                }
+            }
+
+            return user;
+        }
+    }
 };
 
 const handler = NextAuth(authOptions);
 
-export {handler as GET, handler as POST};
-
+export { handler as GET, handler as POST };
--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 
+    // images: {
+    //     domains: ["lh3.googleusercontent.com"],
+    // }
+
     images: {
-        domains: ["lh3.googleusercontent.com"],
-    }
+        remotePatterns: [
+            {   
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '**',
+            }
+        ]
+    } 
 }
 
 module.exports = nextConfig 
